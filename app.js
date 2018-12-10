699a5f12ceadda49de3d236a7651c6a19d185de4
srmooney <srmooney@kent.edu>
2018-12-10 09:31:42 -0500

    set initial value, change new depth to range, clean up

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-// Define a properties array that returns array of objects representing
-// the accepted properties for your application
-var properties = [
-  //{type: 'text', id: "Original Depth"},
-  {type: 'text', id: 'New Depth'}
-];
+function newDepthLabel(args){
+  return 'New Depth ('+ args.preferredUnit +')';
+}
 
-var getSelectedVolumes = function(volumes, selectedVolumeIds){
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function getSelectedVolumes(volumes, selectedVolumeIds){
   return volumes.filter(function(volume){
     return selectedVolumeIds.indexOf(volume.id) >= 0;
   });
+}
+
+// Define a properties function that returns array of objects representing
+// the accepted properties for your application
+var properties = function(args){
+  var depthOriginal = getSelectedVolumes(args.volumes, args.selectedVolumeIds)[0].cut.depth;
+  var step = 0.0001;
+  var max = args.material.dimensions.z;
+  if (args.preferredUnit === 'mm'){
+    depthOriginal = round(depthOriginal * 25.4, 1);
+    max = round(max * 25.4, 1);
+    step = 0.1;
+  }
+  else {
+    depthOriginal = round(depthOriginal, 4);
+  }
+
+  var newDepthInput = {
+    type: 'range', 
+    id: newDepthLabel(args), 
+    value: depthOriginal,
+    min: 0,
+    max: max,
+    step: step
+  };
+
+  return [
+    newDepthInput
+  ];
 };
 
 // Define an executor function that builds an array of volumes,
@@ -17,11 +47,8 @@ var getSelectedVolumes = function(volumes, selectedVolumeIds){
 var executor = function(args, success, failure) {
   var params = args.params;
   var material = args.material;
-  var volumes = [];
-  
-  console.log(args.volumes);
-  
-  var depthNew = parseFloat(params['New Depth']);
+
+  var depthNew = parseFloat(params[newDepthLabel(args)]);
   if (isNaN(depthNew)){ return failure('Please provide a value for "New Depth"'); }
   
   //If user has mm selected covert to in
@@ -38,29 +65,12 @@ var executor = function(args, success, failure) {
   }
   
   var depthOriginal = getSelectedVolumes(args.volumes, args.selectedVolumeIds)[0].cut.depth;
-  
-  
-  //find only with specified depth
-  var selectedVolumes = args.volumes.filter(function(item){ 
-    return item.cut.depth == depthOriginal;
+
+  args.volumes.forEach(function(volume){
+    if (volume.cut.depth == depthOriginal){
+      volume.cut.depth = depthNew;
+    }
   });
 
-  selectedVolumes.forEach(function(volume){
-    //NOTE: have to delete the old one and add new otherwise you 
-    //      get an error about ID's not being unique
-    volumes.push({ id: volume.id });// add id with no shape to delete the existing one
-    delete volume.id;//remove id so it imports as new shape
-    volume.cut.depth = depthNew;
-    volumes.push(volume);
-  });
-  
-  /*
-  console.log({
-    originalVolumes: args.volumes,
-    depthNew,
-    volumes
-  });
-  */
-  
-  return success(volumes);
+  return success(args.volumes);
 };
